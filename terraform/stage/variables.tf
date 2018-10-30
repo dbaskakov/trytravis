f7bb5228fe0a51d2cf74ffd5134d4cf68d1da058
Dmitry <dm.baskakov@yandex.ru>
2018-10-30 13:33:56 +0300

    add stage prod + fmt

---
+++ terraform/stage/variables.tf
@@ -0,0 +1,35 @@
+variable project {
+  description = "infra-219219"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  description = "zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "public key path"
+}
+
+variable private_key_path {
+  description = "private key path"
+}
+
+variable disk_image {
+  description = "reddit-base"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
